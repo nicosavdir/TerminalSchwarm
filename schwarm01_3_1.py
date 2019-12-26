8b71a9bd08a2c4c8254401372a5bf4414e308054
Nico Savdir <online@nicosavdir.com>
2019-12-26 23:24:27 +0100

    STAB

--- schwarm01_3_1.py
+++ schwarm01_3_1.py
@@ -1,4 +1,5 @@
 #letsgoschwarmverhalten
+#IMPORTS________________________________________________________________________
 import time
 import random
 import os
@@ -9,7 +10,99 @@ asciitable=[' ','.','-',':','=','#','@','░','▒','■','▓','█']
 homies=[]
 obstacles=[]
 
+#CLASSES________________________________________________________________________
+class Homie :
+  def __init__(self, id, x, y):
+    self.x = x
+    self.y = y
+    self.id = id
+    self.ascii = 6
+    self.direction = random.randint(0,8)
+    self.do = 0
+    self.visionlen = 5
 
+  def myFunc(self):
+    print("Hello my name is " + self.name)
+
+  def draw(self):
+      #self.drawviewarea()
+      changeGrid(self.x,self.y,self.ascii)
+
+  def random_move(self):
+    dir = random.randint(0,8)
+    self.movedir(dir)
+
+  def movedir(self,dir):
+      #1 2 3
+      #4 0 5
+      #6 7 8
+      if dir==1:
+          self.x-=1
+          self.y-=1
+      elif dir==2:
+          self.y-=1
+      elif dir==3:
+          self.x+=1
+          self.y-=1
+      elif dir==4:
+          self.x-=1
+      elif dir==6:
+          self.x+=1
+      elif dir==6:
+          self.x-=1
+          self.y+=1
+      elif dir==7:
+          self.y+=1
+      elif dir==8:
+          self.x+=1
+          self.y+=1
+
+  def move_avoid(self,ascii):
+      for rows in range(self.visionlen):
+          for cols in range(self.visionlen):
+              print("TODO")
+
+
+  def view(self):
+      viewarray=[]
+      for rows in range(self.visionlen):
+          for cols in range(self.visionlen):
+              g1=int(self.x - int((self.visionlen-1)/2) +cols)
+              g2=int(self.y - int((self.visionlen-1)/2) +rows)
+
+              viewarray.append(getGrid(g1,g2))
+
+              changeGrid(g1,g2,0)
+      return viewarray
+
+  def think(self):
+      viewed=self.view()
+      print(viewed)
+      if viewed.__contains__(10):
+          self.do=10
+
+      if self.do == 0:
+          self.random_move()
+      elif self.do == 10:
+          print("10!")
+
+
+class Obstacle:
+  def __init__(self, id, x, y, height, width, ascii):
+    self.x = x
+    self.y = y
+    self.height = height
+    self.width = width
+    self.id = id
+    self.ascii = 10
+
+  def draw(self):
+      for rows in range(self.height):
+        for cols in range(self.width):
+            changeGrid(self.x+cols,self.y+rows,self.ascii)
+
+
+#FUNCTIONS______________________________________________________________________
 def init():
     grid=[]
     for i in range(size[0]*size[1]):
@@ -55,89 +148,6 @@ def iteratehomies(func):
             homies[numhomies].draw()
 
 
-
-class Homie :
-  def __init__(self, id, x, y):
-    self.x = x
-    self.y = y
-    self.id = id
-    self.ascii = 6
-    self.direction = random.randint(0,8)
-    self.do = 0
-
-  def myFunc(self):
-    print("Hello my name is " + self.name)
-
-  def draw(self):
-      #self.drawviewarea()
-      changeGrid(self.x,self.y,self.ascii)
-
-  def random_move(self):
-    #1 2 3
-    #4 0 5
-    #6 7 8
-    dir = random.randint(0,8)
-
-    if dir==1:
-        self.x-=1
-        self.y-=1
-    elif dir==2:
-        self.y-=1
-    elif dir==3:
-        self.x+=1
-        self.y-=1
-    elif dir==4:
-        self.x-=1
-    elif dir==6:
-        self.x+=1
-    elif dir==6:
-        self.x-=1
-        self.y+=1
-    elif dir==7:
-        self.y+=1
-    elif dir==8:
-        self.x+=1
-        self.y+=1
-
-  def view(self):
-      v_range=5
-      viewarray=[]
-
-      for rows in range(v_range):
-          for cols in range(v_range):
-              g1=int(self.x -2 +cols)
-              g2=int(self.y -2 +rows)
-
-              viewarray.append(getGrid(g1,g2))
-
-              changeGrid(g1,g2,0)
-      return viewarray
-
-  def drawviewarea(self):
-       v_range=5
-       viewarray=[]
-       for rows in range(v_range):
-           for cols in range(v_range):
-               g1=int(self.x -2 +cols)
-               g2=int(self.y -2 +rows)
-               changeGrid(g1,g2,0)
-
-  def think(self):
-      viewed=self.view()
-      print(viewed)
-      for v in range(len(viewed)):
-          if viewed[v]==1:
-              do = 0
-          elif viewed[v]==10:
-              do = 10
-
-      if self.do == 0:
-          self.random_move()
-      elif self.do == 10:
-          time.sleep(10)
-          print("10!")
-
-
 def spawnObstacles(num,ascii):
     for numObstacles in range(num):
         obsSize=[2,2]
@@ -148,21 +158,7 @@ def drawObstacles():
     for numObstacles in range(len(obstacles)):
         obstacles[numObstacles].draw()
 
-class Obstacle:
-  def __init__(self, id, x, y, height, width, ascii):
-    self.x = x
-    self.y = y
-    self.height = height
-    self.width = width
-    self.id = id
-    self.ascii = 10
-
-  def draw(self):
-      for rows in range(self.height):
-        for cols in range(self.width):
-            changeGrid(self.x+cols,self.y+rows,self.ascii)
-
-
+#RUN____________________________________________________________________________
 grid=init()
 
 homies=spawnhomies(10)
@@ -170,10 +166,25 @@ obstacles=spawnObstacles(10,10)
 while True:
     time.sleep(0.1)
 
-    #drawObstacles()
     iteratehomies(0)#THINK
     cleanscreen(0)
     drawObstacles()
     iteratehomies(1)#DRAW
 
     screen(grid)
+
+
+'''TRASHCAN
+
+  def drawviewarea(self):
+       viewarray=[]
+       for rows in range(self.visionlen):
+           for cols in range(self.visionlen):
+               g1=int(self.x - int((visionlen-1)/2) +cols)
+               g2=int(self.y - int((visionlen-1)/2) +rows)
+               changeGrid(g1,g2,0)
+
+
+
+
+'''
